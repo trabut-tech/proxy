7209de24f27a9ab214d44d8d4023d326c9ac6d36
Mohamed Al Ashaal <m7medalash3al@gmail.com>
2023-06-03 19:28:33 +0300

    added auth layer

--- go.mod
+++ go.mod
@@ -2,4 +2,21 @@ module github.com/alash3al/xroxy
 
 go 1.20
 
-require github.com/elazarl/goproxy v0.0.0-20221015165544-a0805db90819 // indirect
+require (
+	github.com/elazarl/goproxy v0.0.0-20221015165544-a0805db90819
+	github.com/labstack/echo/v4 v4.10.2
+)
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/gommon v0.4.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,22 +1,73 @@
 package main
 
 import (
+	"crypto/subtle"
+	"encoding/base64"
 	"flag"
 	"github.com/elazarl/goproxy"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"log"
-	"net/http"
+	"strings"
 )
 
 var (
-	flagListenAddr = flag.String("listen", ":33080", "the http address to start the proxy server on")
-	flagVerbose    = flag.Bool("verbose", true, "whether to be verbose or not")
+	flagListenAddr   = flag.String("listen", ":33080", "the http address to start the proxy server on")
+	flagAuthUser     = flag.String("auth-user", "proxy", "the basic-auth user")
+	flagAuthPassword = flag.String("auth-password", "password", "the basic-auth password")
 )
 
 func main() {
 	flag.Parse()
 
 	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = *flagVerbose
 
-	log.Fatal(http.ListenAndServe(*flagListenAddr, proxy))
+	e := echo.New()
+	e.HideBanner = true
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestID())
+	e.Use(middleware.Logger())
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			proxyAuth := c.Request().Header.Get("Proxy-Authorization")
+			if proxyAuth == "" || !strings.HasPrefix(proxyAuth, "Basic ") {
+				e.Logger.Error("no Proxy-Authorization found")
+				return c.NoContent(401)
+			}
+
+			authDecoded, err := base64.StdEncoding.DecodeString(strings.SplitN(proxyAuth, " ", 2)[1])
+			if err != nil {
+				e.Logger.Errorf("auth base64: %s", err.Error())
+				return c.NoContent(401)
+			}
+
+			keyParts := strings.SplitN(string(authDecoded), ":", 2)
+			if len(keyParts) != 2 {
+				e.Logger.Error("invalid user or password")
+				return c.NoContent(401)
+			}
+
+			user, pass := keyParts[0], keyParts[1]
+
+			if subtle.ConstantTimeCompare([]byte(user), []byte(*flagAuthUser)) != 1 {
+				e.Logger.Error("invalid user specified")
+				return c.NoContent(401)
+			}
+
+			if subtle.ConstantTimeCompare([]byte(pass), []byte(*flagAuthPassword)) != 1 {
+				e.Logger.Error("invalid pass specified")
+				return c.NoContent(401)
+			}
+
+			return next(c)
+		}
+	})
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return echo.WrapHandler(proxy)
+	})
+
+	log.Fatal(e.Start(*flagListenAddr))
 }
